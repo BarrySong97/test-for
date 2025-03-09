c38fb3690bf0221e81f9006adc439f0b60e0ecb5
Barry Song <524000659@qq.com>
2025-03-09 17:40:20 +0800

    feat(image): remotepatterns

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log("middleware", request.url);
+  if (request.url.includes("jpg")) {
+    console.log("middleware", request.url);
+  }
   NextResponse.next();
 }
